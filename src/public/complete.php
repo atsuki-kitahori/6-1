5277f121eb5e7fee496be8b8dcf6eae1b9166af0
atsuki-kitahori <pcp.m.t15@gmail.com>
2024-09-07 12:19:23 +0900

    送信完了ページの処理

--- src/public/complete.php
+++ src/public/complete.php
@@ -1,29 +1,47 @@
 <?php
 $title = $_POST['title'] ?? '';
 $email = $_POST['email'] ?? '';
-$message = $_POST['message'] ?? '';
+$content = $_POST['content'] ?? '';
 
 $errors = [];
-if (empty($title)) {
-    $errors[] = 'タイトルを入力してください。';
-}
-if (empty($email)) {
-    $errors[] = 'メールアドレスを入力してください。';
-}
-if (empty($message)) {
-    $errors[] = '内容を入力してください。';
-}
-
-if (!empty($errors)) {
+if (empty($title) || empty($email) || empty($content)) {
+    $errors = array_filter([
+        empty($title) ? 'タイトルを入力してください。' : null,
+        empty($email) ? 'メールアドレスを入力してください。' : null,
+        empty($content) ? '内容を入力してください。' : null,
+    ]);
     echo 'エラーがあります：<br>';
     foreach ($errors as $error) {
         echo htmlspecialchars($error) . '<br>';
     }
+    echo '<br><a href="index.php">送信画面へ</a>';
+    exit();
 } else {
     echo '<h1>送信完了！！！</h1>';
-    echo 'お問い合わせありがとうございます。以下の内容で受け付けました：<br>';
-    echo 'タイトル: ' . htmlspecialchars($title) . '<br>';
-    echo 'Email: ' . htmlspecialchars($email) . '<br>';
-    echo '内容: ' . htmlspecialchars($message) . '<br>';
+    echo '<br><a href="index.php">送信画面へ</a><br>';
+    echo '<a href="history.php">送信履歴へ</a>';
+
+    // データベースに接続
+    $dbUserName = 'root';
+    $dbPassword = 'password';
+    $pdo = new PDO(
+        'mysql:host=mysql; dbname=contactform; charset=utf8mb4',
+        $dbUserName,
+        $dbPassword
+    );
+
+    // SQLクエリの準備
+    $stmt = $pdo->prepare(
+        'INSERT INTO contacts (title, email, content) VALUES (:title, :email, :content)'
+    );
+
+    // パラメータをバインド
+    $stmt->bindParam(':title', $title, PDO::PARAM_STR);
+    $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+    $stmt->bindParam(':content', $content, PDO::PARAM_STR);
+
+    // クエリを実行
+    $res = $stmt->execute();
 }
+
 ?>
